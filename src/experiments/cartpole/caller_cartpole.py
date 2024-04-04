5941b651424bcf8f54c8cd1b4f3b79025df4e82e
Nicole <orzan.nicole@gmail.com>
2024-04-04 16:29:27 +0200

    fix

--- src/experiments/cartpole/caller_cartpole.py
+++ src/experiments/cartpole/caller_cartpole.py
@@ -1,13 +1,14 @@
 import argparse
 import numpy as np
-from src.experiments.cartpole_train_ppo_single_obj import train_ppo_single_obj
+from src.experiments.cartpole.cartpole_train_ppo_single_obj import train_ppo_single_obj
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_agents', type=int)
         
     parser.add_argument('--num_epochs', type=int, default=50000) # sarebbe total_steps
-    parser.add_argument('--num_game_iterations', type=int, default=1)
+    parser.add_argument('--num_steps', type=int, default=1)
     parser.add_argument('--wandb_mode', type=str, choices = ["online", "offline"], default="offline")
     
     args = parser.parse_args()
